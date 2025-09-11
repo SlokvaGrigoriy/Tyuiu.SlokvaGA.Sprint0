31f2291c218dbff7a3299286d545c2acaed82ce8
Tyuiu.SlokvaGA@outlook.com <grislokva@mail.ru>
2025-09-11 18:02:09 +0500

    Task2

---
+++ Tyuiu.SlokvaGA.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.SlokvaGA.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.SlokvaGA.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTestBase
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Григорий";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет..., Григорий", res);
+        }
+    }
+}
---
+++ Tyuiu.SlokvaGA.Sprint0.Task2.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SlokvaGA.Sprint0.Task2.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Atlas Playbook v0.4.1")]
+[assembly: AssemblyProduct("Tyuiu.SlokvaGA.Sprint0.Task2.V0.Test")]
+[assembly: AssemblyCopyright("Copyright © Atlas Playbook v0.4.1 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8edbd53b-ec10-4dcb-a29b-3bd0acd6c492")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
