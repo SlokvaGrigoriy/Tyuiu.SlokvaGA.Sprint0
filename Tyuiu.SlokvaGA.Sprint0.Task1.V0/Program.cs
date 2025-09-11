c4e2c797828917aef6552fb0193b9d2e83ae874d
Tyuiu.SlokvaGA@outlook.com <grislokva@mail.ru>
2025-09-11 13:55:06 +0500

    age

--- Tyuiu.SlokvaGA.Sprint0.Task1.V0/Program.cs
+++ Tyuiu.SlokvaGA.Sprint0.Task1.V0/Program.cs
@@ -12,7 +12,7 @@ namespace Tyuiu.SlokvaGA.Sprint0.Task1.V0
         {
             Console.WriteLine("Hello, World");
             string name = "Григорий";
-            int age = 45;
+            int age = 18;
             Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет!");
             Console.ReadKey();
         }
