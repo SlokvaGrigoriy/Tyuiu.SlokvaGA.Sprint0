0b286cfd273b153886df8deb0318782450bc9533
Tyuiu.SlokvaGA@outlook.com <grislokva@mail.ru>
2025-09-11 18:51:39 +0500

    Task3

---
+++ Tyuiu.SlokvaGA.Sprint0.Task3.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SlokvaGA.Sprint0.Task3.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Atlas Playbook v0.4.1")]
+[assembly: AssemblyProduct("Tyuiu.SlokvaGA.Sprint0.Task3.V0.Test")]
+[assembly: AssemblyCopyright("Copyright © Atlas Playbook v0.4.1 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("562e6856-2d2c-4a76-a85e-9aa922a4d076")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
